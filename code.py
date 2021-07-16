1a7424efc1dcf1c2d7b3158acc254e3cc1cba61f
Kukry42 <valkuznezov@gmail.com>
2021-07-16 07:32:33 +0300

    works well, with strange error at the end

--- code.py
+++ code.py
@@ -3,17 +3,26 @@ document = docx.Document('table.docx')
 tables = document.tables
 for table in tables:
     shaffeled_rows = list()
+    empty_rows = list()
     for row in table.rows:
-        shaffeled_rows.append(row)
+        row_text_list = list()
+        for cell in row.cells:
+            row_text_list.append(cell.text)
+        if  all(row_text_list):
+            shaffeled_rows.append(row)   
+        else:
+            empty_rows.append(row)
     random.shuffle(shaffeled_rows)
-    new_table = document.add_table(len(table.rows), len(table.columns))
     words_for_cells = list()
     for row in shaffeled_rows:
         for cell in row.cells:
             words_for_cells.append(cell.text)
-    for row in new_table.rows:
+    for row in table.rows:
         for cell in row.cells:
-            cell.text = words_for_cells.pop()
+            try:
+                cell.text = words_for_cells.pop()
+            except:
+                pass
 
 
 
