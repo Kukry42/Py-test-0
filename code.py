4b1cbf5f68eefe93663746af6eec96f51228b0fa
Kukry42 <valkuznezov@gmail.com>
2021-07-18 15:56:07 +0300

    half-empty rows supported

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ for table in tables:
         row_text_list = list()
         for cell in row.cells:
             row_text_list.append(cell.text)
-        if  all(row_text_list):
+        if  bool(''.join(row_text_list)) != False:
             shaffeled_rows.append(row)   
         else:
             empty_rows.append(row)
@@ -20,12 +20,8 @@ for table in tables:
     for row in table.rows:
         for cell in row.cells:
             try:
-                cell.text = words_for_cells.pop()
+                cell.text = words_for_cells.pop(0)
             except:
                 pass
-
-
-
-
 
 document.save('table_shuffeled.docx')
